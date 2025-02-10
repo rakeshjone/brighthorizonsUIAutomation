2d9f4f16295f88ced6faf90cc8501da2d3ca9eb7
Rakesh Chauhan <rakesh_jone@yahoo.co.in>
2025-02-10 22:16:43 +0530

    pseudo test

---
+++ src/test/java/stepDefinitions/searchFeatureStepDefinition.java
@@ -0,0 +1,29 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class searchFeatureStepDefinition {
+    @Given("I navigate bright horizons website")
+    public void iNavigateBrightHorizonsWebsite() {
+    }
+
+    @And("I click on magnifying glass button on home page")
+    public void iClickOnMagnifyingGlassButtonOnHomePage() {
+
+    }
+
+    @And("I enter search text {string}")
+    public void iEnterSearchText(String arg0) {
+    }
+
+    @When("I click on search button")
+    public void iClickOnSearchButton() {
+    }
+
+    @Then("I will see search results contains serach text")
+    public void iWillSeeSearchResultsContainsSerachText() {
+    }
+}
